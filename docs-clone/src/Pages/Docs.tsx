6b831936f709a7755d25ba39a823bd5aa910e4b6
Smruti0109 <125039577+Smruti0109@users.noreply.github.com>
2025-04-16 19:29:41 +0530

    Update Docs.tsx

--- docs-clone/src/Pages/Docs.tsx
+++ docs-clone/src/Pages/Docs.tsx
@@ -1,35 +1,20 @@
-//Docs is our only page....
 import React from "react";
 import ModalComponent from "../Components/Model"
 import { loginWithGoogle } from "../API/Auth";
 import useCheckAuth from "../Hooks/useCheckAuth";
 import Document from "../Components/Document";
-// import Spinner from "../Components/Spinner";
-
 const Docs: React.FC = () => {
   const handleLogin = () => {
     loginWithGoogle();
   };
   const { isAuthenticated, userData } = useCheckAuth();
-
-
   return (
-  
-
         <div className="docs-container">
         {!isAuthenticated ? (
-
-          //meaning if not authenticated then show the login screen
-
-
           <ModalComponent
             title="SignIn"
             handleLogin={handleLogin}
-          ></ModalComponent>
-
-
-
-          
+          ></ModalComponent>         
         
       ) : (
         <>
